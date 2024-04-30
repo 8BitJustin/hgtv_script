0c71498d5b02a4865bb7b19ccd640c73f2672ec4
8bitjustin <j.olson.digital@gmail.com>
2024-04-30 14:24:16 -0700

    Changed from button to form and submit for checkbox screen.

--- main.py
+++ main.py
@@ -24,17 +24,24 @@ url = "https://www.hgtv.com/sweepstakes/hgtv-smart-home/sweepstakes"
 def apply_and_submit(frame, data, submit, enter):
     fr = driver.find_element(By.ID, frame)
     driver.switch_to.frame(fr)
+    print("Frame switched.")
     inp = driver.find_element(By.ID, data)
-    ActionChains(driver) \
-        .scroll_to_element(inp) \
-        .perform()
+    print("Input field focused.")
     inp.click()
     inp.send_keys("justinolson624@gmail.com")
+    print("Email input.")
+    time.sleep(2)
     sub = driver.find_element(By.ID, submit)
     sub.click()
-    time.sleep(10)
-    ent = driver.find_element(By.CLASS_NAME, enter)
-    ent.click()
+    print("Submit clicked.")
+    time.sleep(3)
+    form = driver.find_element(By.ID, enter)
+    if form:
+        print("Enter button found.")
+    else:
+        print("Enter button NOT found.")
+    form.submit()  # As this is a form, I focused on the form ID and not the button. Submit works well for this.
+    print("Enter button clicked.")
     time.sleep(10)
 
 
@@ -58,10 +65,12 @@ try:
     # time.sleep(5)
     # submit = driver.find_element(By.ID, "xCheckUser")
     # submit.click()
-    apply_and_submit("ngxFrame270554", "xReturningUserEmail", "xCheckUser", "xButton")
+    apply_and_submit("ngxFrame270554", "xReturningUserEmail", "xCheckUser", "xSecondaryForm")
     print("HGTV Submitted.")
     time.sleep(10)
     food_link = driver.find_element(By.CLASS_NAME, "editorial-link-no-style")
+    # https://www.hgtv.com/sweepstakes/hgtv-smart-home/sweepstakes/thanks is the link that should be clicked, not
+    # working as it's not found.
     food_link.click()
     # Should direct to https://www.foodnetwork.com/sponsored/sweepstakes/hgtv-smart-home-sweepstakes?xp=sistersite
     time.sleep(10)
